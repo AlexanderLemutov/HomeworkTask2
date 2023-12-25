5ffaab49c6e81bfd1f6efc4d3e2ecd30ce016385
AlexanderLemutov <148871154+AlexanderLemutov@users.noreply.github.com>
2023-12-25 13:28:47 +0300

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,7 +5,7 @@ public class Main {
         testArray.addElement("TheLongestWord");
         testArray.addElement("LongerWord");
         testArray.addElement("Word");
-        testArray.addInElement();
+        testArray.addInElement(); //добавление элемента, введенного с клавиатуры
         testArray.printArray();
         System.out.println(testArray.getMaxElement());
         System.out.println(testArray.getAverageLen());
