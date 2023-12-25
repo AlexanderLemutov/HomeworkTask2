d60252ca969a978c5c0ee478e63c3db82b747ed2
AlexanderLemutov <148871154+AlexanderLemutov@users.noreply.github.com>
2023-12-25 04:02:19 +0300

    Main.java added

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+public class Main {
+    public static void main(String[] args) {
+        StringArray testArray = new StringArray();
+        testArray.addElement("TheLongestWord");
+        testArray.addElement("LongerWord");
+        testArray.addElement("Word");
+        testArray.printArray();
+        System.out.println(testArray.getMaxElement());
+        System.out.println(testArray.getAverageLen());
+    }
+}
